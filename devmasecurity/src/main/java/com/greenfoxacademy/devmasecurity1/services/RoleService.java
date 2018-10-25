51f2337506cf13d580ddfedfd666c2864ae72868
Hegyi Bence <hegyibence94@gmail.com>
2018-10-25 22:29:49 +0200

    Roles can be added by admin

--- devmasecurity/src/main/java/com/greenfoxacademy/devmasecurity1/services/RoleService.java
+++
@@ -1,8 +0,0 @@
-package com.greenfoxacademy.devmasecurity1.services;
-
-import com.greenfoxacademy.devmasecurity1.model.ClientRole;
-
-public interface RoleService {
-
-  ClientRole findClientRoleByName(String name);
-}
